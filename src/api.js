d92395c36c2be5288a1358e5debd790c71efeac2
User <eduardvitko@gmail.com>
2025-07-20 23:47:38 +0300

    fix api.js

--- src/api.js
+++ src/api.js
@@ -6,14 +6,14 @@ const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL
 });
 
-// Додаємо "перехоплювач", який автоматично додає токен авторизації до кожного запиту
-api.interceptors.request.use(config => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
+// // Додаємо "перехоплювач", який автоматично додає токен авторизації до кожного запиту
+// api.interceptors.request.use(config => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+// });
 
 /* ================== API-функції ================== */
 
