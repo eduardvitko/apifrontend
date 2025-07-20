7bfef353df3385f2d09d38c5e95f06fe24698dd2
User <eduardvitko@gmail.com>
2025-07-20 21:59:38 +0300

    change const ApiClient in api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const API = axios.create({
-    baseURL: 'http://localhost:8080/api',
+// const API = axios.create({
+//     baseURL: 'http://localhost:8080/api',
+// });
+const apiClient = axios.create({
+    // Замінюємо статичну адресу на змінну середовища
+    baseURL: process.env.REACT_APP_API_URL
 });
-
 export const loginUser = (data) => API.post('/auth/login', data);
 export const registerUser = (data) => API.post('/auth/register', data);
 export const fetchCategories = () => API.get('/categories/all');
