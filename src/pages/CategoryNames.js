1aa146f516a6520ea59533f6800a7ba26e3a525f
User <eduardvitko@gmail.com>
2025-08-01 13:06:21 +0300

    names category for translate

--- src/pages/CategoryNames.js
+++ src/pages/CategoryNames.js
@@ -1,12 +1,12 @@
 
 const CategoryNames = {
-    5: { ua: 'Бейсболки', en: 'baseball caps' },
-    3: { ua: 'Взуття', en: 'Shoes' },
-    6: { ua: 'Спортивні куртки', en: '\n' +
-            'sports jackets' },
-    4: { ua: 'Спортивні товари', en: 'Sport products' },
-    1: { ua: 'Футболки', en: '\n' +
-            't-shirts' },
+    5: { ua: 'Картошка фри', en: 'french fries ' },
+    3: { ua: 'Бургери', en: 'Burgers' },
+    6: { ua: 'ролли', en: '\n' +
+            'rolls' },
+    4: { ua: 'Піцца', en: 'Pizza' },
+    1: { ua: 'Суші', en: '\n' +
+            'sushi' },
     // додай всі категорії, які є у тебе на бекенді
 };
 
